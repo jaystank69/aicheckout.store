d39766036e775fb5620495655e4ff07fa4db3eff
jaystank69 <joakim5a@hotmail.com>
2025-11-10 22:02:44 +0100

    Create stories.js

---
+++ data/stories.js
@@ -0,0 +1,30 @@
+[
+  {
+    "user": "Daily Drop",
+    "avatar": "/assets/img/avatar-01.webp",
+    "type": "image",
+    "src": "/assets/stories/story-01.webp",
+    "duration": 5000
+  },
+  {
+    "user": "Loop Lab",
+    "avatar": "/assets/img/avatar-02.webp",
+    "type": "video",
+    "src": "/assets/stories/story-02.mp4",
+    "duration": 7000
+  },
+  {
+    "user": "Color Pop",
+    "avatar": "/assets/img/avatar-03.webp",
+    "type": "image",
+    "src": "/assets/stories/story-03.webp",
+    "duration": 5000
+  },
+  {
+    "user": "Sneak Peek",
+    "avatar": "/assets/img/avatar-01.webp",
+    "type": "video",
+    "src": "/assets/stories/story-04.mp4",
+    "duration": 7000
+  }
+]
